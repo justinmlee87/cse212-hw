e4cf80ffbd419b94c83e419692aedeed2d04be65
Justin Lee <justinmlee87@gmail.com>
2024-06-13 12:52:22 -0400

    HW

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -1,8 +1,10 @@
-public static class ArraysTester {
+public static class ArraysTester
+{
     /// <summary>
     /// Entry point for the tests
     /// </summary>
-    public static void Run() {
+    public static void Run()
+    {
         // Sample Test Cases (may not be comprehensive)
         Console.WriteLine("\n=========== PROBLEM 1 TESTS ===========");
         double[] multiples = MultiplesOf(7, 5);
@@ -34,14 +36,20 @@ public static class ArraysTester {
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     private static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Store multiples in an empty array
+        double[] multiples = new double[length];
 
-        return new double[0]; // replace this return statement with your own
+        // Create a loop to loop through the multiples
+        for (int i = 0; i < length; i++)
+        {
+            // Calculate then add multiples to the array
+            multiples[i] = number * (i + 1);
+        }
+
+        // Return the array of multiples
+        return multiples;
     }
-    
+
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
     /// List{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
@@ -52,10 +60,18 @@ public static class ArraysTester {
     /// </summary>
     private static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Set the effective rotation amount
+        int effectiveAmount = amount % data.Count;
+
+        // Take out the elements to be rotated
+        List<int> rotatedElements = data.GetRange(data.Count - effectiveAmount, effectiveAmount);
+
+        // Remove the elements from the original list
+        data.RemoveRange(data.Count - effectiveAmount, effectiveAmount);
+
+        // Insert the extracted elements at the beginning of the original list
+        data.InsertRange(0, rotatedElements);
+
 
     }
 }
